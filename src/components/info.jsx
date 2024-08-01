dc9ac9987536d46aaae92eec3f3f7c410a2fe2cf
Cezar Trevino <datdood@Cezars-MacBook-Pro.local>
2024-07-31 22:02:48 -0400

    changed path of images/SVGs again

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -2,19 +2,19 @@ import '/src/App.css';
 export default function Info() {
     return (
         <>
-        <img src="/public/images/Selfie.png" className="selfie"/>
+        <img src="/images/Selfie.png" className="selfie"/>
         <h1 className="name">Cezar Breeman Trevino</h1>
         <h2 className="job">React Developer</h2>
         <div className="social-btns">
             <div className="email">
                 <button className="email-btn">
-                <img src="/public/assets/email-1572-svgrepo-com.svg" className="svg-info email-svg"/>
+                <img src="/assets/email-1572-svgrepo-com.svg" className="svg-info email-svg"/>
                     Email
                 </button>
             </div>
             <div className="linkedin">
                 <button className="linkedin-btn">
-                    <img src="/public/assets/linkedin-svgrepo-com.svg" className="svg-info linkedin-svg"/>
+                    <img src="/assets/linkedin-svgrepo-com.svg" className="svg-info linkedin-svg"/>
                     Linkedin
                 </button>
             </div>
