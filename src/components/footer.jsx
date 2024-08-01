7252798adc777eb70f1282461a71724fa593e0e9
Cezar Trevino <datdood@Cezars-MacBook-Pro.local>
2024-07-31 22:00:10 -0400

    changed path of images/SVGs

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,8 @@ import '/src/App.css';
 export default function Footer() {
     return (
         <div className="footer">
-            <img src="/src/assets/social-facebook-svgrepo-com.svg" className="svg-footer facebook"/>
-            <img src="/src/assets/github-rounded-svgrepo-com.svg" className="svg-footer github"/>
+            <img src="/public/assets/social-facebook-svgrepo-com.svg" className="svg-footer facebook"/>
+            <img src="/public/assets/github-rounded-svgrepo-com.svg" className="svg-footer github"/>
         </div>
     )
 }
